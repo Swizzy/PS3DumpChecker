5254e042d5609823e945f478dcb65bba8ec2d0da
littlebalup <littlebalup@gmail.com>
2019-09-12 11:18:15 +0200

    update to build 497

--- src/PS3DumpChecker/Properties/AssemblyInfo.cs
+++ src/PS3DumpChecker/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.496.0")]
-[assembly: AssemblyFileVersion("1.0.496.0")]
+[assembly: AssemblyVersion("1.0.497.0")]
+[assembly: AssemblyFileVersion("1.0.497.0")]
